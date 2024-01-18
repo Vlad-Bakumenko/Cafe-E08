d0ecf0b65b36d646b95fff611fbbd67ffab1d0d2
Tezmoideo <jsmoerps@gmail.com>
2024-01-18 10:10:36 +0000

    menu class created

---
+++ main.js
@@ -0,0 +1,49 @@
+class Menu {
+    constructor(type, name, price, isWarm) {
+      this.type = type;
+      this.name = name;
+      this.price = price;
+      this.isWarm = isWarm;
+    }
+}
+
+const food1 = new Menu("food", "Club Sandwich", 8.0, true);
+
+
+const drink1 = new Menu("drink", "Water", 1.5, false);
+
+
+const food2 = new Menu("food", "Chocolate Cake", 2.5, true);
+
+
+const drink2 = new Menu("drink", "Beer", 3.5, false);
+
+
+const food3 = new Menu("food", "Sweet Hamburger", 11.5, true);
+
+
+const drink3 = new Menu("drink", "Wine", 5.5, false);
+
+
+
+
+
+
+console.log(food1); 
+
+console.log(drink1);
+
+console.log(food2); 
+
+console.log(drink2);
+
+console.log(food3); 
+
+console.log(drink3);
+
+
+
+
+
+
+
