95ac252d31003d6b4ad578fc116125c2856fc4c1
Vladyslav Bakumenko <vladyslav.bakumenko@dci-student.org>
2024-01-18 10:25:24 +0100

    Worker class created

---
+++ main.js
@@ -0,0 +1,20 @@
+class Worker {
+    constructor(name, position) {
+        this.name = name;
+        this.position = position;
+    }
+}
+
+const worker1 = new Worker("John", "Cashier");
+const worker2 = new Worker("Giovanni", "Barista");
+const worker3 = new Worker("Sandy", "Cashier");
+const worker4 = new Worker("Bob", "Barista");
+const worker5 = new Worker("Katy", "Cashier");
+const worker6 = new Worker("Lily", "Barista");
+
+console.log(worker1);
+console.log(worker2);
+console.log(worker3);
+console.log(worker4);
+console.log(worker5);
+console.log(worker6);
